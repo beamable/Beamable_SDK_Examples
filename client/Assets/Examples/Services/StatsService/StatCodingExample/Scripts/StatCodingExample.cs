32b63baf29c1175e218a58fd56a8962fa6c8c585
srivello <srivello@hotmail.com>
2021-02-25 13:35:36 -0600

    Update folders and namespaces

---
+++ client/Assets/Examples/Services/StatsService/StatCodingExample/Scripts/StatCodingExample.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Beamable.Examples.Services.Stats
+{
+   public class StatCodingExample : MonoBehaviour
+   {
+      //  Unity Methods   ------------------------------
+      protected void Start()
+      {
+         Beamable.API.Instance.Then(beamableAPI =>
+         {
+            UseStatCoding(beamableAPI);
+         });
+      }
+
+      //  Other Methods   ------------------------------
+      private async void UseStatCoding(IBeamableAPI beamableAPI)
+      {
+         string statKey = "MyExampleStat";
+         string access = "public";
+         string domain = "client";
+         string type = "player";
+         long id = beamableAPI.User.id;
+
+         // Set Value
+         Dictionary<string, string> setStats =
+            new Dictionary<string, string>() { { statKey, "99" } };
+
+         await beamableAPI.Stats.SetStats(access, setStats);
+
+         // Get Value
+         Dictionary<string, string> getStats = 
+            await beamableAPI.Stats.GetStats(domain, access, type, id);
+
+         string myExampleStatValue = "";
+         getStats.TryGetValue(statKey, out myExampleStatValue);
+         Debug.Log("myExampleStatValue: " + myExampleStatValue);
+      }
+   }
+}
