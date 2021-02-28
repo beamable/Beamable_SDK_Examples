6ec114958d2305b52fb93ea66ff957bf4ad4fc7f
srivello <srivello@hotmail.com>
2021-02-28 15:35:42 -0600

    Make Template

---
+++ client/Assets/Examples/Features/[TemplateExample]/Scripts/Template.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace Beamable.Examples.Features.XXX
+{
+    /// <summary>
+    /// Demonstrates <see cref="Example"/>.
+    /// </summary>
+    public class Example : MonoBehaviour
+    {
+        protected void Start()
+        {
+            Debug.Log("Start()");
+        }
+    }
+}
