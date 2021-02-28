b8834476747b763c441e0f5acc43284b27b07041
srivello <srivello@hotmail.com>
2021-02-28 14:54:47 -0600

    Add AdminFlow Example

---
+++ client/Assets/Examples/Features/AdminFlow/Scripts/AdminFlowCustomCommandExample.cs
@@ -0,0 +1,32 @@
+﻿using Beamable.ConsoleCommands;
+using UnityEngine;
+
+namespace Beamable.Examples.Features.AdminFlow2
+{
+    [BeamableConsoleCommandProvider]
+    public class CustomConsoleCommandProvider 
+    {
+        [BeamableConsoleCommand ("Add", "A sample addition command", "Add <int> <int>")]
+        public string Add(string[] args)
+        {
+            var a = int.Parse(args[0]);
+            var b = int.Parse(args[1]);
+            return "result: " + (a + b);
+        }
+    }
+    
+    /// <summary>
+    /// Demonstrates <see cref="AdminFlow"/>.
+    /// </summary>
+    public class AdminFlowCustomCommandExample : MonoBehaviour
+    {
+        protected void Start()
+        {
+            Debug.Log("Start() Instructions...\n" + 
+                      " * Run The Scene\n" + 
+                      " * Type '~' in Unity Game Window\n" + 
+                      " * Type 'Add 5 10`\n" + 
+                      " * See results in Unity Console Window\n");
+        }
+    }
+}
