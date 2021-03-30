94b6ca6954fbcc22e58fb37acd7f4366dedb3c84
srivello <srivello@beamable.com>
2021-03-30 10:49:09 -0600

    Add TournamentFlow Example

---
+++ client/Assets/Examples/Features/TournamentFlow/Scripts/TournamentFlowExample.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+namespace Beamable.Examples.Features.TournamentFlow
+{
+    /// <summary>
+    /// Demonstrates <see cref="TournamentFlow"/>.
+    /// </summary>
+    public class TournamentFlowExample : MonoBehaviour
+    {
+        //  Properties  -----------------------------------
+        //  Fields  ---------------------------------------
+        //  Unity Methods  --------------------------------
+
+        protected void Start()
+        {
+            Debug.Log("Start()");
+
+            SetupBeamable();
+        }
+        
+        //  Methods  --------------------------------------
+        private async void SetupBeamable()
+        {
+            var beamableAPI = await Beamable.API.Instance;
+
+            Debug.Log($"beamableAPI.User.id = {beamableAPI.User.id}");
+        }
+        
+        
+        //  Event Handlers  -------------------------------
+    }
+}
