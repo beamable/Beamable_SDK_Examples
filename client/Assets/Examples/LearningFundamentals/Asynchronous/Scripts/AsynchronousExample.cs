7bd913b78bfb97cd6ef492ee9054c29678da2ea0
srivello <srivello@beamable.com>
2021-03-31 12:45:33 -0600

    Add LearningFundamentals

---
+++ client/Assets/Examples/LearningFundamentals/Asynchronous/Scripts/AsynchronousExample.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Beamable.Examples.LearningFundamentals
+{
+    /// <summary>
+    /// Demonstrates asynchronous programming.
+    /// </summary>
+    public class AsynchronousExample : MonoBehaviour
+    {
+        //  Unity Methods  --------------------------------
+        protected void Start()
+        {
+            Debug.Log("Start()");
+
+            MyMethodViaAsyncAwait();
+            MyMethodViaCallback();
+            StartCoroutine(MyMethodViaCoroutine());
+        }
+        
+        //  Methods  --------------------------------------
+        private async void MyMethodViaAsyncAwait()
+        {
+            var beamableAPI = await Beamable.API.Instance;
+            Debug.Log($"beamableAPI.User.id = {beamableAPI.User.id}");
+        }
+    
+        private void MyMethodViaCallback()
+        {
+            Beamable.API.Instance.Then(beamableAPI =>
+            {
+                Debug.Log($"beamableAPI.User.id = {beamableAPI.User.id}");
+            });
+        }
+    
+        private IEnumerator MyMethodViaCoroutine()
+        {
+            var promise = Beamable.API.Instance;
+            yield return promise.ToYielder();
+            var beamableAPI = promise.GetResult();
+    
+            Debug.Log($"beamableAPI.User.id = {beamableAPI.User.id}");
+        }
+        
+        //  Event Handlers  -------------------------------
+    }
+}
