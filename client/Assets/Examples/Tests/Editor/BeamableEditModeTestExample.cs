23aae8f48597b2ce46e4353c3470dd1fab037c8b
srivello <srivello@beamable.com>
2021-04-01 15:39:57 -0600

    Added 2 working example tests and related asmdefs

---
+++ client/Assets/Examples/Tests/Editor/BeamableEditModeTestExample.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using NUnit.Framework;
+using UnityEngine.TestTools;
+
+namespace Beamable.Examples.Tests
+{
+    /// <summary>
+    /// Demonstrates a working example of an EditMode test.
+    /// </summary>
+    public class BeamableEditModeTestExample
+    {
+        [UnityTest]
+        public IEnumerator UserId_LengthIs16_WhenGetInstance()
+        {
+            // Arrange
+            var promise = Beamable.API.Instance;
+            yield return promise.ToYielder();
+            var beamableAPI = promise.GetResult();
+            
+            // Act
+            int userIdLength = beamableAPI.User.id.ToString().Length;
+
+            // Assert
+            Assert.That(userIdLength, Is.EqualTo(16));
+        }
+    }
+}
---
+++ client/Assets/Examples/Tests/Runtime/BeamablePlayModeTestExample.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using NUnit.Framework;
+using UnityEngine.TestTools;
+
+namespace Beamable.Examples.Tests
+{
+    /// <summary>
+    /// Demonstrates a working example of a PlayMode test.
+    /// </summary>
+    public class BeamablePlayModeTestExample
+    {
+        [UnityTest]
+        public IEnumerator UserId_LengthIs16_WhenGetInstance()
+        {
+            // Arrange
+            var promise = Beamable.API.Instance;
+            yield return promise.ToYielder();
+            var beamableAPI = promise.GetResult();
+            
+            // Act
+            int userIdLength = beamableAPI.User.id.ToString().Length;
+
+            // Assert
+            Assert.That(userIdLength, Is.EqualTo(16));
+        }
+    }
+}
