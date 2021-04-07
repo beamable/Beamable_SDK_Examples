8672bf7506ad5cedd6df407d4523e4df9ff7b6d0
srivello <srivello@beamable.com>
2021-04-07 16:40:03 -0600

    Add EventsService Example

---
+++ client/Assets/Examples/Runtime/Services/InventoryService/Scripts/EventsServiceExampleUI.cs
@@ -0,0 +1,88 @@
+using System.Text;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Beamable.Examples.Services.EventsService
+{
+   /// <summary>
+   /// The UI for the <see cref="EventsServiceExample"/>.
+   /// </summary>
+   public class EventsServiceExampleUI : MonoBehaviour
+   {
+      //  Fields  ---------------------------------------
+      
+      [SerializeField] private EventsServiceExample _eventsServiceExample = null;
+
+      [SerializeField] private TMP_Text _text01 = null;
+      [SerializeField] private TMP_Text _text02 = null;
+
+      [SerializeField] private Button _setScoreButton = null;
+      [SerializeField] private Button _claimButton = null;
+      [SerializeField] private Button _refreshAllButton = null;
+      
+      //  Unity Methods  --------------------------------
+      protected void Start()
+      {
+         _eventsServiceExample.OnRefreshed.AddListener(EventsServiceExample_OnRefreshed);
+         _setScoreButton.onClick.AddListener(Add1ItemButton_OnClicked);
+         _claimButton.onClick.AddListener(Delete1ItemButton_OnClicked);
+         _refreshAllButton.onClick.AddListener(RefreshAllButton_OnClicked);
+      }
+
+      //  Methods  --------------------------------------
+      
+      private void Add1ItemButton_OnClicked()
+      {
+         _eventsServiceExample.ClaimRewardsInEvents();
+      }
+
+
+      private void Delete1ItemButton_OnClicked()
+      {
+         _eventsServiceExample.SetScoreInEvents();
+      }
+
+      private void RefreshAllButton_OnClicked()
+      {
+         _eventsServiceExample.Refresh();
+      }
+      
+      //  Event Handlers  -------------------------------
+      private void EventsServiceExample_OnRefreshed(EventsServiceExampleData 
+         eventsServiceExampleData)
+      {
+         StringBuilder stringBuilder01 = new StringBuilder();
+         
+         // Show UI: Events
+         foreach (string runningEventsLog in eventsServiceExampleData.RunningEventsLogs)
+         {
+            stringBuilder01.Append(runningEventsLog).AppendLine();
+         }
+         _text01.text = stringBuilder01.ToString();
+         
+         StringBuilder stringBuilder02 = new StringBuilder();
+         
+         // Show UI: Scores
+         stringBuilder02.Append("SCORES").AppendLine();
+         foreach (string setScoreLog in eventsServiceExampleData.SetScoreLogs)
+         {
+            stringBuilder02.Append(setScoreLog).AppendLine();
+         }
+         
+         // Show UI: Claims
+         stringBuilder02.Append("CLAIMS").AppendLine();
+         foreach (string claimLog in eventsServiceExampleData.ClaimLogs)
+         {
+            stringBuilder02.Append(claimLog).AppendLine();
+         }
+         _text02.text = stringBuilder02.ToString();
+
+         // Show UI: Button Content Names
+         _setScoreButton.GetComponentInChildren<TMP_Text>().text = $"SetScore\n({eventsServiceExampleData.Score})";
+         _claimButton.GetComponentInChildren<TMP_Text>().text = $"Claim";
+      }
+   }
+}
+
+
