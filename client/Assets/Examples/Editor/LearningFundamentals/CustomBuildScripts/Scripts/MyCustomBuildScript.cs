f298cb19163891c14b78366566ee05b880b9b709
srivello <srivello@beamable.com>
2021-05-22 15:49:26 -0600

    Add CustomBuildScripts example

---
+++ client/Assets/Examples/Editor/LearningFundamentals/CustomBuildScripts/Scripts/MyCustomBuildScript.cs
@@ -0,0 +1,66 @@
+﻿using UnityEditor;
+using UnityEditor.Callbacks;
+using UnityEngine;
+#if UNITY_IOS
+using UnityEditor.iOS.Xcode;
+#endif
+
+namespace Beamable.Examples.LearningFundamentals.CustomBuildScripts
+{
+    /// <summary>
+    /// Demonstrates CustomBuildScripts programming.
+    ///
+    /// The following is an arbitrary example. Replace with
+    /// whatever are the specific project needs.
+    ///
+    /// Usage
+    ///  * Stop The Scene
+    ///  * Unity -> File -> Build And Run 
+    ///  * See results in the Unity Console Window
+    /// 
+    /// </summary>
+    public static class MyCustomBuildScript 
+    {
+        //  Unity Methods  --------------------------------
+        
+        /// <summary>
+        /// Runs after compiling Unity and before native platform (such as Xcode for iOS).
+        /// </summary>
+        [PostProcessBuild]
+        public static void OnPostProcessBuild(BuildTarget target, string path)
+        {
+            Debug.Log($"MyCustomBuildScript.cs, OnPostProcessBuild({target})");
+#if UNITY_IOS
+            OnPostProcessBuild_iOS (target, path);
+#endif
+        }
+        
+        //  Methods  --------------------------------------
+#if UNITY_IOS
+      private static void OnPostProcessBuild_iOS(BuildTarget target, string path)
+      {
+         if (target == BuildTarget.iOS && AccountManagementConfiguration.Instance.EnableGoogleSignInOnApple)
+         {
+            UpdateAppleProjectSettings(path);
+         }
+      }
+
+      private static void UpdateAppleProjectSettings(string path)
+      {
+         var pbxPath = PBXProject.GetPBXProjectPath(path);
+         var project = new PBXProject();
+         project.ReadFromFile(pbxPath);
+
+#if UNITY_2019_1_OR_NEWER
+         var target = project.GetUnityMainTargetGuid();
+#else
+         var target = project.TargetGuidByName(PBXProject.GetUnityTargetName());
+#endif
+
+         project.AddBuildProperty(target, "OTHER_LDFLAGS", "-ObjC");
+
+         File.WriteAllText(pbxPath, project.WriteToString());
+      }
+#endif
+    }
+}
