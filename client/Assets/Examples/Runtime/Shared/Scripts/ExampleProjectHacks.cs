e9dfbac6f0f98c41aee1be2fc3d970a3396bc3b8
srivello <srivello@beamable.com>
2021-05-25 17:45:54 -0600

    Add reusable hacks for demo purposes

---
+++ client/Assets/Examples/Runtime/Shared/Scripts/ExampleProjectHacks.cs
@@ -0,0 +1,24 @@
+﻿using Beamable.Api;
+using Beamable.Service;
+
+namespace Beamable.Examples.Shared
+{ 
+    public static class ExampleProjectHacks
+    {
+
+        //  Methods  --------------------------------------
+        
+        /// <summary>
+        /// Clears all data related to the active runtime user(s)
+        /// 
+        /// HACK: This is not recommended for production usage
+        /// 
+        /// </summary>
+        public static void ClearDeviceUsersAndReloadScene()
+        {
+            PlatformService platformService = ServiceManager.Resolve<PlatformService>();
+            platformService.ClearDeviceUsers();
+            ServiceManager.OnTeardown();
+        }
+    }
+}
