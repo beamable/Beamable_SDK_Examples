369063f5fb685435928ee4921559579ef15d6c8c
Samuel Asher Rivello <74617375+srivelloBeamable@users.noreply.github.com>
2021-07-19 09:47:13 -0600

    Add ConnectivityService Example

---
+++ client/Assets/Examples/Runtime/Services/ConnectivityService/Scripts/ConnectivityServiceExample.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Beamable.Examples.Services.ConnectivityService
+{
+    /// <summary>
+    /// Holds data for use in the <see cref="ConnectivityServiceExampleUI"/>.
+    /// </summary>
+    [System.Serializable]
+    public class ConnectivityServiceExampleData
+    {
+        public List<string> OutputLogs = new List<string>();
+        public bool HasConnectivity = false;
+    }
+   
+    [System.Serializable]
+    public class RefreshedUnityEvent : UnityEvent<ConnectivityServiceExampleData> { }
+    
+    /// <summary>
+    /// Demonstrates <see cref="ConnectivityService"/>.
+    /// </summary>
+    public class ConnectivityServiceExample : MonoBehaviour
+    {
+        //  Events  ---------------------------------------
+        [HideInInspector]
+        public RefreshedUnityEvent OnRefreshed = new RefreshedUnityEvent();
+        
+        //  Fields  ---------------------------------------
+        private IBeamableAPI _beamableAPI = null;
+        private ConnectivityServiceExampleData _data = new ConnectivityServiceExampleData();
+    
+        //  Unity Methods  --------------------------------
+        protected void Start()
+        {
+            Debug.Log($"Start() Instructions...\n" +
+                      " * Ensure Computer's Internet Is Active\n" +
+                      " * Run The Scene\n" +
+                      " * See Onscreen UI Show HasInternet = true\n" +
+                      " * Ensure Computer's Internet Is NOT Active\n" +
+                      " * See Onscreen UI Show HasInternet = false\n");
+
+            SetupBeamable();
+        }
+        
+        //  Methods  --------------------------------------
+        private async void SetupBeamable()
+        {
+            _beamableAPI = await Beamable.API.Instance;
+
+            Debug.Log($"beamableAPI.User.id = {_beamableAPI.User.id}");
+
+            // Observe ConnectivityService Changes
+            _beamableAPI.ConnectivityService.OnConnectivityChanged += ConnectivityService_OnConnectivityChanged;
+            
+            // Update UI Immediately
+            bool hasConnectivity = _beamableAPI.ConnectivityService.HasConnectivity;
+            ConnectivityService_OnConnectivityChanged(hasConnectivity);
+        }
+        
+        public void ToggleHasInternet()
+        {
+            _beamableAPI.ConnectivityService.SetHasInternet(!_data.HasConnectivity);
+        }
+
+        public void Refresh()
+        {
+            string refreshLog = $"Refresh() ..." +
+                                $"\n * HasInternet = {_data.HasConnectivity}" + 
+                                $"\n * OutputLogs = {_data.OutputLogs.Count}\n\n";
+            
+            //Debug.Log(refreshLog);
+            
+            // Send relevant data to the UI for rendering
+            OnRefreshed?.Invoke(_data);
+        }
+        
+        //  Event Handlers  -------------------------------
+        private void ConnectivityService_OnConnectivityChanged(bool hasConnectivity)
+        {
+            _data.HasConnectivity = hasConnectivity;
+            
+            _data.OutputLogs.Add($"HasConnectivity = {_data.HasConnectivity}");
+            
+            Refresh();
+        }
+    }
+}
---
+++ client/Assets/Examples/Runtime/Services/ConnectivityService/Scripts/ConnectivityServiceExampleUI.cs
@@ -0,0 +1,78 @@
+using System.Text;
+using Beamable.Examples.Shared;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Beamable.Examples.Services.ConnectivityService
+{
+   /// <summary>
+   /// The UI for the <see cref="ConnectivityServiceExample"/>.
+   /// </summary>
+   public class ConnectivityServiceExampleUI : ExampleCanvasUI
+   {
+      //  Fields  ---------------------------------------
+      [SerializeField] private ConnectivityServiceExample _connectivityServiceExample = null;
+
+      // Menu Panel
+      private TMP_Text MenuTitleText { get { return TitleText01; }}
+      private Button SetHasInternetButton { get { return Button01;}}
+      
+      // Groups Panel
+      private TMP_Text MainTitleText { get { return TitleText02; }}
+      private TMP_Text MainBodyText { get { return BodyText02; }}
+      
+      // Messages Panel 
+      private TMP_Text LogsTitleText { get { return TitleText03; }}
+      private TMP_Text LogsBodyText { get { return BodyText03; }}
+      
+      //  Unity Methods  --------------------------------
+      protected void Start()
+      {
+         _connectivityServiceExample.OnRefreshed.AddListener(GroupsServiceExample_OnRefreshed);
+         SetHasInternetButton.onClick.AddListener(SetHasInternetButton_OnClicked);
+         
+         // Populate default UI
+         _connectivityServiceExample.Refresh();
+      }
+
+      //  Methods  --------------------------------------
+      
+      
+      //  Event Handlers  -------------------------------
+      private async void SetHasInternetButton_OnClicked()
+      {
+         _connectivityServiceExample.ToggleHasInternet();
+      }
+
+      
+      private void GroupsServiceExample_OnRefreshed(ConnectivityServiceExampleData 
+         connectivityServiceExampleData)
+      {
+         // Show UI: HasInternet
+         StringBuilder stringBuilder01 = new StringBuilder();
+         stringBuilder01.Append("HAS INTERNET").AppendLine();
+         stringBuilder01.Append($" • HasInternet = {connectivityServiceExampleData.HasConnectivity}").AppendLine();
+         MainBodyText.text = stringBuilder01.ToString();
+         
+         // Show UI: Group Players
+         StringBuilder stringBuilder02 = new StringBuilder();
+         stringBuilder02.Append("LOGS").AppendLine();
+         foreach (string log in connectivityServiceExampleData.OutputLogs)
+         {
+            stringBuilder02.Append($" • {log}").AppendLine();
+         }
+         LogsBodyText.text = stringBuilder02.ToString();
+         
+         // Show UI: Other 
+         MenuTitleText.text = "Connectivity Service Example";
+         MainTitleText.text = "Main";
+         LogsTitleText.text = "Logs";
+         
+         SetHasInternetButton.GetComponentInChildren<TMP_Text>().text =
+            $"Debug\nSet HasInternet = {!connectivityServiceExampleData.HasConnectivity}";
+      }
+   }
+}
+
+
