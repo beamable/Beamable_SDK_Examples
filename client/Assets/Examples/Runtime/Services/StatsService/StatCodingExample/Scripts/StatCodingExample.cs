9728e51368e9de86be1f0087e5d33eb304a9c0e5
Samuel Asher Rivello <74617375+srivelloBeamable@users.noreply.github.com>
2021-07-22 12:50:11 -0600

    Update StatCodingExample.cs

--- client/Assets/Examples/Runtime/Services/StatsService/StatCodingExample/Scripts/StatCodingExample.cs
+++ client/Assets/Examples/Runtime/Services/StatsService/StatCodingExample/Scripts/StatCodingExample.cs
@@ -3,46 +3,46 @@ using UnityEngine;
 
 namespace Beamable.Examples.Services.StatsService
 {
-/// <summary>
-/// Demonstrates <see cref="StatsService"/>.
-/// </summary>
-public class StatCodingExample : MonoBehaviour
-{
-//  Unity Methods  --------------------------------
-protected void Start()
-{
-Debug.Log($"Start()");
+    /// <summary>
+    /// Demonstrates <see cref="StatsService"/>.
+    /// </summary>
+    public class StatCodingExample : MonoBehaviour
+    {
+        //  Unity Methods  --------------------------------
+        protected void Start()
+        {
+            Debug.Log($"Start()");
 
-SetupBeamable();
-}
+            SetupBeamable();
+        }
 
-//  Other Methods   ------------------------------
-private async void SetupBeamable()
-{
-var beamableAPI = await Beamable.API.Instance;
+        //  Other Methods   ------------------------------
+        private async void SetupBeamable()
+        {
+            var beamableAPI = await Beamable.API.Instance;
 
-Debug.Log($"beamableAPI.User.id = {beamableAPI.User.id}");
+            Debug.Log($"beamableAPI.User.id = {beamableAPI.User.id}");
 
-string statKey = "MyExampleStat";
-string access = "public";
-string domain = "client";
-string type = "player";
-long id = beamableAPI.User.id;
+            string statKey = "MyExampleStat";
+            string access = "public";
+            string domain = "client";
+            string type = "player";
+            long id = beamableAPI.User.id;
 
-// Set Value
-Dictionary<string, string> setStats =
-    new Dictionary<string, string>() { { statKey, "99" } };
+            // Set Value
+            Dictionary<string, string> setStats =
+                new Dictionary<string, string>() { { statKey, "99" } };
 
-await beamableAPI.StatsService.SetStats(access, setStats);
+            await beamableAPI.StatsService.SetStats(access, setStats);
 
-// Get Value
-Dictionary<string, string> getStats =
-    await beamableAPI.StatsService.GetStats(domain, access, type, id);
+            // Get Value
+            Dictionary<string, string> getStats =
+                await beamableAPI.StatsService.GetStats(domain, access, type, id);
 
-string myExampleStatValue = "";
-getStats.TryGetValue(statKey, out myExampleStatValue);
+            string myExampleStatValue = "";
+            getStats.TryGetValue(statKey, out myExampleStatValue);
 
-Debug.Log($"myExampleStatValue = {myExampleStatValue}");
-}
-}
+            Debug.Log($"myExampleStatValue = {myExampleStatValue}");
+        }
+    }
 }
