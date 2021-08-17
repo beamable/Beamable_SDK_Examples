d1d55f8c1f7a457c30226608c12d56f7c316e8d0
Samuel Asher Rivello <74617375+srivelloBeamable@users.noreply.github.com>
2021-08-17 14:03:44 -0600

    Update leaderboard example

--- client/Assets/Examples/Runtime/Services/LeaderboardService/Scripts/LeaderboardServiceExample.cs
+++ client/Assets/Examples/Runtime/Services/LeaderboardService/Scripts/LeaderboardServiceExample.cs
@@ -12,35 +12,47 @@ namespace Beamable.Examples.Services.LeaderboardService
         //  Fields  ---------------------------------------
         [SerializeField] private LeaderboardRef _leaderboardRef = null;
         [SerializeField] private double _score = 100;
-
+        private IBeamableAPI beamableAPI = null;
+        
         //  Unity Methods  --------------------------------
         protected async void Start()
         {
             Debug.Log($"Start()");
 
-            await LeaderboardServiceSetScore(_leaderboardRef.Id, _score);
-            await LeaderboardServiceGetScore(_leaderboardRef.Id);
+ 
+            SetupBeamable();
         }
 
         //  Methods  --------------------------------------
+        private async void SetupBeamable()
+        {
+            beamableAPI = await Beamable.API.Instance;
+            
+            Debug.Log($"beamableAPI.User.id = {beamableAPI.User.id}");
+            
+            // Set
+            Debug.Log($"LeaderboardService.SetScore({_leaderboardRef.Id},{_score})");
+            await LeaderboardServiceSetScore(_leaderboardRef.Id, _score);
+            
+            // Get
+            double score = await LeaderboardServiceGetScore(_leaderboardRef.Id);
+            Debug.Log($"LeaderboardService.GetScore({_leaderboardRef.Id},{score})");
+        }
+        
+        
         private async Task LeaderboardServiceSetScore(string id, double score)
         {
-            var beamableAPI = await Beamable.API.Instance;
-
-            Debug.Log($"beamableAPI.User.id = {beamableAPI.User.id}");
-
             await beamableAPI.LeaderboardService.SetScore(id, score);
-
-            Debug.Log($"LeaderboardService.SetScore({id},{score})");
         }
 
-        private async Task LeaderboardServiceGetScore(string id)
+        
+        private async Task<double> LeaderboardServiceGetScore(string id)
         {
-            var beamableAPI = await Beamable.API.Instance;
             var score = await beamableAPI.LeaderboardService
                 .GetUser(id, beamableAPI.User.id)
                 .Map(entry => entry.score);
-            Debug.Log($"LeaderboardService.GetScore({id},{score})");
+
+            return score;
         }
     }
 }
