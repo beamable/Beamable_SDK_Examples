df1f11bce9acacfe57331edffd143cab236b2a58
Samuel Asher Rivello <74617375+srivelloBeamable@users.noreply.github.com>
2021-09-08 12:12:30 -0600

    Add account management flow example

---
+++ client/Assets/Examples/Runtime/Prefabs/AccountManagementFlow/Scripts/AccountManagementFlowExample.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace Beamable.Examples.Prefabs.AccountManagementFlow
+{
+    /// <summary>
+    /// Demonstrates <see cref="AccountManagementFlow"/>.
+    /// </summary>
+    public class AccountManagementFlowExample : MonoBehaviour
+    {
+        //  Unity Methods  --------------------------------
+        protected void Start()
+        {
+            Debug.Log($"Start() Instructions...\n\n" + 
+                      " * Run The Scene\n" + 
+                      "\n");
+        }
+    }
+}
