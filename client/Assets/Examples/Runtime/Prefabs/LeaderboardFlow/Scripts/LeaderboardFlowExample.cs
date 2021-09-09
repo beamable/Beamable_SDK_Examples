7e8f64ce362f1d8d415009dc53e88259b90b3637
Samuel Asher Rivello <74617375+srivelloBeamable@users.noreply.github.com>
2021-09-08 20:32:04 -0600

    Update leaderboards

--- client/Assets/Examples/Runtime/Prefabs/LeaderboardFlow/Scripts/LeaderboardFlowExample.cs
+++ client/Assets/Examples/Runtime/Prefabs/LeaderboardFlow/Scripts/LeaderboardFlowExample.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
 
 namespace Beamable.Examples.Prefabs.LeaderboardFlow
 {
@@ -7,9 +9,18 @@ namespace Beamable.Examples.Prefabs.LeaderboardFlow
     /// </summary>
     public class LeaderboardFlowExample : MonoBehaviour
     {
+        [SerializeField] 
+        private List<GameObject> _gameObjectsToHide = null;
+        
         //  Unity Methods  --------------------------------
         protected void Start()
         {
+            foreach (GameObject go in _gameObjectsToHide)
+            {
+                //Hide back button - not needed for this demo
+                go.SetActive(false);
+            }
+            
             Debug.Log($"Start() Instructions...\n\n" +
                       " * Run The Scene\n" +
                       " * See UI representing leaderboard on-screen\n" +
