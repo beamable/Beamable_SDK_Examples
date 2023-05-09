5ba852cc541815cfd713f60afcf00e63d11a9abb
Allister MacLeod <allister@beamable.com>
2023-05-09 09:06:35 -0400

    Fixing content type hierarchy comment (#33)
    * Expand doc comment for Armor content type.
    
    Point out that armor will be nested under items as items.armor.
    
    * Expand doc comment on ComplexItem for content type.
    
    Same note about nesting as on the Armor content type.

--- client/Assets/Examples/Runtime/Shared/Scripts/Armor.cs
+++ client/Assets/Examples/Runtime/Shared/Scripts/Armor.cs
@@ -6,13 +6,17 @@ namespace Beamable.Examples.Shared
 {
     [Serializable]
     public class ArmorLink : ContentLink<Armor> {}
-    
+
     [Serializable]
     public class ArmorRef : ContentRef<Armor> {}
-    
+
     /// <summary>
     /// This type defines a custom ItemContent for use in
-    /// several example scenes
+    /// several example scenes.
+    ///
+    /// Note that content types are hierarchical. Because this is a subclass of
+    /// ItemContent, it also inherits "items" as a parent content type. The
+    /// resulting content type will be "items.armor", nested under "items".
     /// </summary>
     [ContentType("armor")]
     public class Armor : ItemContent
